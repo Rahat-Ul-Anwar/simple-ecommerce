88d39ed5fef2f0616cc31f3ee8dc8432b47d0445
Rahat-Ul-Anwar <anwarrahat69@gmail.com>
2022-09-28 10:56:20 +0600

    cart desing

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,10 +9,11 @@ const Header = () => {
             <nav className="menu">
                 <img src={logo} alt="" srcset="" />
                 <div className='nav-menu'>
-                    <a href="/order">Order</a>
-                    <a href="/review">Order Review</a>
-                    <a href="/inventory">Manage Inventory</a>
-                    <a href="/login">Login</a>
+                        <a href="/order">Order</a>
+                        <a href="/review">Order Review</a>
+                        <a href="/inventory">Manage Inventory</a>
+                        <a href="/login">Login</a>
+                        <a href="/blog">Blog</a>
                 </div>
            </nav>
     
